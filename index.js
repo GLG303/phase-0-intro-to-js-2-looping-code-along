97d2c94fd4e887426dcf23d09c7bcb767b0f728b
Garrett Grigsby <loulemew@gmail.com>
2023-05-23 20:46:15 -0600

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const newArray = []
+function writeCards(array, string){
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(integer){
+    let countDown = 0;
+    while (integer >= 0){
+    console.log(integer--)
+    }
+}
